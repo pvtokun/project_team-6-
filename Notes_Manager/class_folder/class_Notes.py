84fff6dd11c16ff7e3fc26e39bdb2274f1e67342
Vitalyi <evciu97@gmail.com>
2023-06-04 19:01:03 +0300

    здати на перевірку.

---
+++ Notes_Manager/class_folder/class_Notes.py
@@ -0,0 +1,37 @@
+class Note:
+    def __init__(self, title, content, tags):
+        self.title = title # Назввание
+        self.content = content # Содержание
+        self.tags = tags # Теги
+
+# класс NoteBook определим для управления заметками
+
+class NoteBook:
+    def __init__(self):
+        self.notes = []
+
+    def add_note(self, note):
+        self.notes.append(note)
+
+    def edit_note(self, note_index):
+        if note_index < len(self.notes):
+            note = self.notes[note_index]
+            new_title = input(f"Enter new title for the note '{note.title}': ")
+            new_content = input(f"Enter new content for the note '{note.content}': ")
+            new_tags = input(f"Enter new comma-separated tags for the note '{', '.join(note.tags)}': ").split(",")
+            note.title = new_title
+            note.content = new_content
+            note.tags = new_tags
+            return "Note edited successfully!"
+        else:
+            return "Invalid note index!"
+
+    def search_notes_by_tag(self, tag):
+        result = []
+        for note in self.notes:
+            if tag in note.tags:
+                result.append(note)
+        return result
+    
+    def get_all_notes(self):
+        return self.notes
---
+++ Notes_Manager/function_folder/func_Notes.py
@@ -0,0 +1,61 @@
+# определим класс Note для заметок
+from ..class_folder.class_Notes import Note, NoteBook
+
+notebook = NoteBook()
+
+def input_error(func):
+    def inner(*args):
+        try:
+            result = func(*args)  # Викликати функцію з введеними аргументами
+            return result
+        except KeyError:
+            return "No user"  # Обробка винятку KeyError
+        except ValueError:
+            return 'Give me name and phone please'  # Обробка винятку ValueError
+        except IndexError:
+            return 'Enter user name'  # Обробка винятку IndexError
+        except TypeError:
+            return 'Missed arguments'  # Обробка винятку TypeError
+    return inner
+
+@input_error
+def add_note():
+    title = input("Enter note title: ")
+    content = input("Enter note content: ")
+    tags = input("Enter comma-separated tags for the note: ").split(",")
+    note = Note(title, content, tags)
+    notebook.add_note(note)
+    return "Note added successfully!"
+
+@input_error
+def search_notes():
+    tag = input("Enter tag to search notes: ")
+    matching_notes = notebook.search_notes_by_tag(tag)
+    if matching_notes:
+        result = "Matching notes:\n"
+        for note in matching_notes:
+            result += f"Title: {note.title}\nContent: {note.content}\nTags: {', '.join(note.tags)}\n\n"
+    else:
+        result = "No notes found with the given tag."
+    return result
+
+@input_error
+def edit_note():
+    note_index = int(input("Enter the index of the note to edit: "))
+    result = notebook.edit_note(note_index)
+    return result
+
+@input_error
+def delete_note(note_index):
+    pass
+    
+@input_error
+def show_notes():
+    notes = notebook.get_all_notes()
+    if notes:
+        result = "Notes:\n"
+        for note in notes:
+            result += f"Title: {note.title}\nContent: {note.content}\nTags: {', '.join(note.tags)}\n\n"
+    else:
+        result = "No notes found."
+    return result
